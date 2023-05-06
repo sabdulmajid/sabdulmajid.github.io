497575eaaed8cd9975799af90e4ce880a4d6040f
sabdulmajid <ayman.hasib@outlook.com>
2023-05-06 15:48:57 +0300

    gh-pages

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/gatsby-icon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"d7d8bb0f2bbbc296c844635e3e6a6ddc"},
+    }]
